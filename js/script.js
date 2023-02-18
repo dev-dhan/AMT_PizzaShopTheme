c0b1086ec24be6f9c23bea11517f04162957ed94
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-02-18 22:35:31 +1100

    Add go up button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ hamburger.onclick = function() {
     navBar = document.querySelector(".nav-links");
     navBar.classList.toggle("active");
 }
+
+
+/**Go Up Button */
+$(document).ready(function(){
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+    $('#topBtn').fadeIn();
+    } else{
+    $('#topBtn').fadeOut();
+    }
+});
+
+$("#topBtn").click(function(){
+    $('html ,body').animate({scrollTop : 0},800);
+});
+});
